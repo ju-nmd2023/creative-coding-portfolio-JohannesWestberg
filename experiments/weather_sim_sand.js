1f6fcb2d4687afc9149f73bd88532d1d6e2923c6
JohannesWestberg <117350232+JohannesWestberg@users.noreply.github.com>
2026-01-12 03:14:37 +0100

    Update weather_sim_sand.js

--- experiments/weather_sim_sand.js
+++ experiments/weather_sim_sand.js
@@ -1,9 +1,9 @@
 let particles = [];
 
-let particleAmount = 8;
+let particleAmount = 11;
 let speed = 0;
 let durationMin = 80;
-let durationMax = 600;
+let durationMax = 400;
 
 // band which spawns particles is now flipped
 let bandWidth = 25;    
